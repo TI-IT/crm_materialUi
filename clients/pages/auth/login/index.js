55322868d37afba8c4266386d6939705049c281e
go_projects_og <go.TIIT.ogle@gmail.com>
2023-01-20 02:05:05 +0300

    001

--- clients/pages/auth/login/index.js
+++ clients/pages/auth/login/index.js
@@ -15,66 +15,37 @@ const LoginPage = () => {
     const { layoutConfig } = useContext(LayoutContext);
     const contextPath = getConfig().publicRuntimeConfig.contextPath;
     const router = useRouter();
-    const containerClassName = classNames('surface-ground flex align-items-center justify-content-center min-h-screen min-w-screen overflow-hidden', { 'p-input-filled': layoutConfig.inputStyle === 'filled' });
+    const containerClassName = classNames('surface-ground flex align-items-center justify-content-center min-h-screen min-w-screen overflow-hidden', {'p-input-filled': layoutConfig.inputStyle === 'filled'});
 
-    const [user, setUser] = React.useState({ email: '', password: '' });
-    const [message, setMessage] = React.useState('');
-
-    function changeUser(name, value) {
-        setUser({
-            ...user,
-            [name]: value
-        });
-    }
-
-    async function login() {
-        setMessage('');
-        if (!user.email || !user.password) {
-            setMessage('Заполните оба поля');
-        }
-        const res = await fetch(server_host + '/users/login', {
-            method: 'post',
-            credentials: 'include',
-            body: JSON.stringify(user),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-        const data = await res.json();
-        if (data.ok) {
-            setMessage('Сейчас будет выполнена переадресация');
-            router.push('/dashboard');
-        } else {
-            setMessage('Ошибка попробуйте другие данные');
-        }
-    }
     return (
         <div className={containerClassName}>
             <div className="flex flex-column align-items-center justify-content-center">
-                {/* <img src={`${contextPath}/layout/images/logo-${layoutConfig.colorScheme === 'light' ? 'dark' : 'white'}.svg`} alt="Sakai logo" className="mb-5 w-6rem flex-shrink-0"/> */}
+                <img src={`${contextPath}/layout/images/logo-${layoutConfig.colorScheme === 'light' ? 'dark' : 'white'}.svg`} alt="Sakai logo" className="mb-5 w-6rem flex-shrink-0"/>
                 <div style={{ borderRadius: '56px', padding: '0.3rem', background: 'linear-gradient(180deg, var(--primary-color) 10%, rgba(33, 150, 243, 0) 30%)' }}>
                     <div className="w-full surface-card py-8 px-5 sm:px-8" style={{ borderRadius: '53px' }}>
                         <div className="text-center mb-5">
                             <img src={`${contextPath}/demo/images/login/avatar.png`} alt="Image" height="50" className="mb-3" />
-                            <div className="text-900 text-3xl font-medium mb-3">Добро пожаловать в Алюмаком</div>
-                            <span className="text-600 font-medium">Войдите, чтобы продолжить</span>
+                            <div className="text-900 text-3xl font-medium mb-3">Welcome, Isabel!</div>
+                            <span className="text-600 font-medium">Sign in to continue</span>
                         </div>
 
                         <div>
                             <label htmlFor="email1" className="block text-900 text-xl font-medium mb-2">
                                 Email
                             </label>
-                            <InputText inputid="email1" type="text" placeholder="Email address" className="w-full md:w-30rem mb-5" style={{ padding: '1rem' }} onChange={(e) => changeUser('email', e.target.value)} value={user.email} />
+                            <InputText inputid="email1" type="text" placeholder="Email address" className="w-full md:w-30rem mb-5" style={{ padding: '1rem' }} />
 
                             <label htmlFor="password1" className="block text-900 font-medium text-xl mb-2">
                                 Password
                             </label>
-                            <Password inputid="password1" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" toggleMask className="w-full mb-5" inputClassName="w-full p-3 md:w-30rem"></Password>
+                            <Password inputid="password1" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" toggleMask className="w-full mb-5" inputClassName='w-full p-3 md:w-30rem'></Password>
 
                             <div className="flex align-items-center justify-content-between mb-5 gap-5">
                                 <div className="flex align-items-center">
                                     <Checkbox inputid="rememberme1" checked={checked} onChange={(e) => setChecked(e.checked)} className="mr-2"></Checkbox>
-                                    <label htmlFor="rememberme1">Remember me</label>
+                                    <label htmlFor="rememberme1">
+                                        Remember me
+                                    </label>
                                 </div>
                                 <a className="font-medium no-underline ml-2 text-right cursor-pointer" style={{ color: 'var(--primary-color)' }}>
                                     Forgot password?
